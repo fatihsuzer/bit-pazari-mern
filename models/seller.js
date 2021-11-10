8df08bb1ff7921af58e00bb3a352faa045a0d59c
Fatih Suzer <maclabam@gmail.com>
2021-11-10 11:50:47 +0300

    remove comment lines from seller model

--- models/seller.js
+++ models/seller.js
@@ -11,27 +11,3 @@ const SellerSchema = new mongoose.Schema({
 const Seller = mongoose.model('Seller', SellerSchema);
 
 export default { Seller };
-
-// class Seller {
-//   constructor(id = uuidv4(), name, allProducts = [], comments = []) {
-//     this.id = id;
-//     this.name = name;
-//     this.allProducts = allProducts;
-//     this.comments = comments;
-//   }
-
-//   async addProduct(productHeader, price) {
-//     const product = Product.create({
-//       productHeader: productHeader,
-//       seller: this,
-//       price: price,
-//     });
-//     this.allProducts.push(product);
-//     await productDatabase.insert(product);
-//     return product;
-//   }
-
-//   static create({ id, name, allProducts, comments }) {
-//     return new Seller(id, name, allProducts, comments);
-//   }
-// }
